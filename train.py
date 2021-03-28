ff7dfde58f9568fac3f56f94f58cabc049733abb
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-28 16:05:07 +0800

    '3,28'

--- train.py
+++ train.py
@@ -5,19 +5,26 @@ from transformers import BertTokenizer, BertModel
 import torch.nn as nn
 import numpy as np
 import time
+import argparse
+
+parser = argparse.ArgumentParser("Text-cls training task.")
+parser.add_argument("--epoch", type=int, default= 10)
+logger = util.create_logger("Bert Training", log_file='/logfile')
+args = parser.parse_args()
 
 
 def main():
+    logger.info("Loading data...")
     tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
     train_loader = util.profile('train.txt', tokenizer)
-    test_loader = util.profile('test.txt', tokenizer)
+    # test_loader = util.profile('test.txt', tokenizer)
     dev_loader = util.profile('dev.txt', tokenizer)
-
+    logger.info("Build model...")
     bert = BertModel.from_pretrained('bert-base-chinese')
     model = fn_cls(bert)
     criterion = nn.BCELoss()
     sigmoid = nn.Sigmoid()
-    max_epoch = 10
+    max_epoch = args.epoch
     model.cuda()
     model.train()
     optim = torch.optim.Adam(model.parameters(), lr=1e-5)
@@ -42,10 +49,10 @@ def main():
             loss.backward()
             optim.step()
             optim.zero_grad()
-        print('Train Epoch: {} \tLoss:{:.6f} Acc:{}'.format(
+        logger.info('Train Epoch: {} \tLoss:{:.6f} Acc:{}'.format(
             epoch, np.mean(losslis), 100 * np.mean(tacc)
         ))
-        print('time:{}'.format((time.time() - train_time)))
+        logger.info('time:{}'.format((time.time() - train_time)))
         # evaluate epoch
         model.eval()
         li = []
@@ -63,8 +70,8 @@ def main():
             acc.append(np.mean((pred == target).cpu().numpy()))
             eval_loss = criterion(sigmoid(output).view(-1, 10), target)
             li.append(eval_loss.item())
-        print('-' * 50)
-        print('Validate Epoch: {} acc:{:.2f}%\tLoss:{:.6f}'.format(
+        logger.info('-' * 50)
+        logger.info('Validate Epoch: {} acc:{:.2f}%\tLoss:{:.6f}'.format(
             1, np.mean(acc) * 100, np.mean(li)))
 
 
