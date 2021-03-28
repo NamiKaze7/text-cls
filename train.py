d25514e06f8253af74032469238df4132a41a3cd
NamiKaze7 <57282567+NamiKaze7@users.noreply.github.com>
2021-03-28 12:55:01 +0800

    '3,28'

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import util
 from transformers import BertTokenizer, BertModel
 import torch.nn as nn
 import numpy as np
+import time
 
 
 def main():
@@ -22,6 +23,9 @@ def main():
 
     for epoch in range(1, max_epoch + 1):
         tacc = []
+        losslis = []
+        # train epoch
+        train_time = time.time()
         for batch_idx, (data, target) in enumerate(train_loader):
             data.cuda()
             target.cuda()
@@ -33,10 +37,35 @@ def main():
             pred = torch.argmax(output, 1)
             tacc.append(np.mean((pred == torch.argmax(target, 1)).cpu().numpy()))
             loss = criterion(sigmoid(output).view(-1, 10), target)
+            losslis.append(loss.item())
             loss.backward()
             optim.step()
             optim.zero_grad()
-        print(np.mean(tacc))
+        print('Train Epoch: {} \tLoss:{:.6f} Acc:{}'.format(
+            epoch, np.mean(losslis), 100 * np.mean(tacc)
+        ))
+        print('time:{}'.format((time.time() - train_time)))
+        # evaluate epoch
+        model.eval()
+        li = []
+        acc = []
+        for batch_idx, (data, target) in enumerate(dev_loader):
+            data, target = torch.tensor(data).cuda(), torch.tensor(
+                target).cuda()
+            mask = []
+            for sample in data:
+                mask.append([1 if i != 0 else 0 for i in sample])
+            mask = torch.tensor(mask).cuda()
+
+            output = model(data, attention_mask=mask)
+            pred = torch.argmax(output, 1)
+            acc.append(np.mean((pred == target).cpu().numpy()))
+            eval_loss = criterion(sigmoid(output).view(-1, 10), target)
+            li.append(eval_loss.item())
+        print('-' * 50)
+        print('Validate Epoch: {} acc:{:.2f}%\tLoss:{:.6f}'.format(
+            1, np.mean(acc) * 100, np.mean(li)))
+
 
 
 if __name__ == "__main__":
